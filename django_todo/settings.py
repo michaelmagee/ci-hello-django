8c81efa0624e87cebf3828b4e36d02789c36a679
michaelmagee <59846293+michaelmagee@users.noreply.github.com>
2020-11-27 14:11:10 -0500

    Set secret key from env variable

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 import dj_database_url
 
@@ -21,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '&)*^od=umb8gcne#!_&^1_a=xu#u_=%h2iwyrgxo5c&i-fq*0-'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['magee-django-todo-app.herokuapp.com']
+ALLOWED_HOSTS = ['magee-django-todo-app.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
